0509a34eca36e45f26ea8bfcee9449629ce467c5
BT_4N2_21 <jackn25@nycstudents.net>
2023-09-19 13:04:27 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My name is Jack Ni.");
         System.out.println("my favorite movie is Avengers: Endgame.");
+        System.out.println("My favorite food is sushi.");
     }
 }
