f808cd0390443ce44741acbfd94ff439e881df4b
unknown <BT_4N2_21@BKTECH.LOCAL>
2023-09-19 12:43:55 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Jack Ni.");
+        System.out.println("my favorite movie is Avengers: Endgame.");
+    }
+}
